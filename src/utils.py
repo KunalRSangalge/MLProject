66442e282fe61c3d43d137be5757fa48eb00e0a9
KunalRS <kunal.sangalge@gmail.com>
2025-11-04 19:07:58 +0530

    hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -16,6 +16,7 @@ from sklearn.ensemble import RandomForestRegressor,AdaBoostRegressor
 from sklearn.svm import SVR
 from catboost import CatBoostRegressor 
 from xgboost import XGBRegressor
+from sklearn.model_selection import GridSearchCV
 
 
 def save_object(file_path,obj):
@@ -30,14 +31,20 @@ def save_object(file_path,obj):
         raise CustomException(e,sys)
     
 
-    
-def evaluate_model(X_train,y_train,X_test,y_test,models):
+
+def evaluate_model(X_train,y_train,X_test,y_test,models,param):
     try:
         reports = {}
 
         for i in range (len(list(models))):
             model = list(models.values())[i]
+            para = param[(list(models.keys())[i])]
+            # model.fit(X_train,y_train)
 
+            gs = GridSearchCV(model,para,cv=5,n_jobs=-1)
+            gs.fit(X_train,y_train)
+            
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
 
             y_train_pred = model.predict(X_train)
